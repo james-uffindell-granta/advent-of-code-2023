927dab674750958175abe79da3f2576ba537ce37
James Uffindell-Birt <james.uffindell-birt@ansys.com>
2023-12-06 09:37:12 +0000

    Add an alternative approach based on quadratic formula

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,5 +1,4 @@
-
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 pub struct Race {
     time: u64,
     distance: u64,
@@ -19,9 +18,26 @@ pub fn parse_input_single(input: &str) -> Race {
     Race { time, distance }
 }
 
-pub fn part_1(races: &[Race]) -> usize {
+pub fn solve(races: &[Race]) -> usize {
     races.iter()
-        .map(|r| (0..=r.time).filter_map(|h| (h * (r.time - h) > r.distance).then_some(())).count())
+        .map(|r|
+            (0..=r.time)
+                // formula for the distance: if you hold the button for h seconds, you go at h speed for (r.time-h) time
+                .filter_map(|h| (h * (r.time - h) > r.distance).then_some(()))
+                .count())
+        .product()
+}
+
+pub fn solve_quadratic(races: &[Race]) -> u64 {
+    races.iter()
+        .map(|r| {
+            // quadratic equation time
+            let lower_root = (r.time as f64 - ((r.time * r.time - 4 * r.distance) as f64).sqrt()) / 2.0;
+            // need to be careful: if the lower root is already an int, we need to skip it - so floor and add 1
+            let lowest_value_working = lower_root.floor() as u64 + 1;
+            // there are then that many values below that don't work (counting 0), and the same at the top by symmetry
+            (r.time + 1).saturating_sub(lowest_value_working * 2)
+        })
         .product()
 }
 
@@ -29,8 +45,9 @@ pub fn main() {
     let input = include_str!("../input.txt");
     let races = parse_input(input);
     let single_race = parse_input_single(input);
-    println!("Part 1: {}", part_1(&races));
-    println!("Part 2: {}", part_1(&[single_race]));
+    println!("Part 1: {}", solve(&races));
+    println!("Part 2: {}", solve(&[single_race])); // 1.4s, 33ms in release
+    println!("Part 2 (quad): {}", solve_quadratic(&[single_race])); // about 500us, 200us in release
 }
 
 #[test]
@@ -38,9 +55,9 @@ pub fn test() {
     let input = r"Time:      7  15   30
 Distance:  9  40  200";
 
-    let races = dbg!(parse_input(input));
-    assert_eq!(part_1(&races), 288);
+    let races = parse_input(input);
+    assert_eq!(solve(&races), 288);
     let single_race = parse_input_single(input);
-
-    assert_eq!(part_1(&[single_race]), 71503);
+    assert_eq!(solve(&[single_race]), 71503);
+    assert_eq!(solve_quadratic(&races), 288);
 }
